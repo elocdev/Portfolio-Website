3b1ec7582c253f9479b1b1c78e7d23e74bb620f7
Eric Loc <eloc1385@gmail.com>
2022-07-11 20:03:07 -0700

    color theme menu fixed

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+var styleSwitchToggler = document.querySelector(".style-switcher-toggler");
+styleSwitchToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+const alternateStyles = document.querySelector(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else {
+            style.removeAttribute("disabled", "true");
+        }
+    })
+}
