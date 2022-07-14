202258f8038b56a0f90edc60a424a0aa542f0f77
Eric Loc <eloc1385@gmail.com>
2022-07-14 06:54:09 -0700

    highlighting active side bar

--- scripts/script.js
+++ scripts/script.js
@@ -49,6 +49,9 @@ const nav = document.querySelector(".nav"),
         for(let i=0; i<totalNavList; i++){
             const a = navList[i].querySelector("a");
             a.addEventListener("click", function(){
+                for(let j=0; j<totalNavList; j++){
+                    navList[j].querySelector("a").classList.remove("active")
+                }
                 this.classList.add("active")
             })
         }
